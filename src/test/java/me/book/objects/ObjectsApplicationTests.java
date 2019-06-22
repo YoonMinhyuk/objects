f885003cf01b4c46b6df6a5100972e240677776b
YoonMinhyuk <ymh92730@gmail.com>
2019-06-23 05:11:10 +0900

    init commit

---
+++ src/test/java/me/book/objects/ObjectsApplicationTests.java
@@ -0,0 +1,16 @@
+package me.book.objects;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ObjectsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
